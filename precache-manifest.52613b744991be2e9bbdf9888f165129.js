0a738a781bc94c56ae46c094c5aa866a51e47917
jarfar <krabiksinij@gmail.com>
2018-11-15 10:53:57 +0200

    Initial commit

---
+++ precache-manifest.52613b744991be2e9bbdf9888f165129.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8a4315ebfa523a54a3dc",
+    "url": "/static/js/main.8a4315eb.chunk.js"
+  },
+  {
+    "revision": "e30a718556498d72272e",
+    "url": "/static/js/1.e30a7185.chunk.js"
+  },
+  {
+    "revision": "8a4315ebfa523a54a3dc",
+    "url": "/static/css/main.57eada36.chunk.css"
+  },
+  {
+    "revision": "057c16cfc60884cc1113d3e59484c2c8",
+    "url": "/index.html"
+  }
+];
